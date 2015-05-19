9f50ba7d23758aa11fd61e53120a4e5fd9cfc66a
hongxun.lhx <hongxun.lhx@alibaba-inc.com>
2015-05-19 18:55:02 +0800

    add support for js in demos

--- _site/assets/js/demos.js
+++ _site/assets/js/demos.js
@@ -1,6 +1,21 @@
 $(function () {
   'use strict';
-  window.addEventListener('push', function(e) {
-    console.log(e);
-  });
+
+  var initPageBtns = function() {
+    //初始化页面的代码放在这里
+    console.log("btns page init");
+  }
+
+  var initPages = function() {
+    // 在 content 上加id，通过id判断加载的是哪一个页面。
+    var $content = $(".content");
+    var id = $content[0].id;
+    console.log(id);
+    if(id == 'page-btns') {
+      initPageBtns();
+    }
+  }
+  $(window).on('push', initPages);
+
+  initPages();  //如果是刷新当前页面，没有push事件。
 });
