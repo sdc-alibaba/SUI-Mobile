5b45b7065d57782aecbe8167de7ed231ff53f37c
hongxun.lhx <hongxun.lhx@alibaba-inc.com>
2015-06-16 19:54:51 +0800

    add demos

--- docs/assets/js/demos.js
+++ docs/assets/js/demos.js
@@ -130,6 +130,36 @@ $(function () {
     });
   });
 
+  $(document).on('click','.create-actions', function () {
+    var buttons1 = [
+      {
+        text: '请选择',
+        label: true
+      },
+      {
+        text: '卖出',
+        bold: true,
+        color: 'danger',
+        onClick: function() {
+          $.alert("你选择了“卖出“");
+        }
+      },
+      {
+        text: '买入',
+        onClick: function() {
+          $.alert("你选择了“买入“");
+        }
+      }
+    ];
+    var buttons2 = [
+      {
+        text: '取消',
+        bg: 'danger'
+      }
+    ];
+    var groups = [buttons1, buttons2];
+    $.actions(groups);
+  }); 
 
   //加载提示符
   $(document).on('click','.open-preloader-title', function () {
