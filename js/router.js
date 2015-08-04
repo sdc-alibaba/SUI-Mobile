0da79bef6daf201a61bf62ea5a697e34d2b03476
hongxun.lhx <hongxun.lhx@alibaba-inc.com>
2015-08-04 18:22:08 +0800

    update router

--- js/router.js
+++ js/router.js
@@ -3,23 +3,30 @@
   "use strict";
 
   var Router = function() {
-    window.addEventListener('popstate', $.proxy(this.onpopstate, this));
     this.state = sessionStorage;
     this.state.setItem("stateid", this.state.getItem("stateid") || 1);
     this.state.setItem("currentStateID", this.state.getItem("stateid"));
-
+    this.urlIDMap = {};
+    this.init();
+  }
+  Router.prototype.init = function() {
     var currentPage = this.getCurrentPage();
+    if(!currentPage[0]) currentPage = $(".page").eq(0).addClass("page-current");
     var hash = location.hash;
+    if(currentPage[0] && !currentPage[0].id) currentPage[0].id = (hash ? hash.slice(1) : this.genRandomID());
 
-    if(hash && (!currentPage[0] || hash.slice(1) !== currentPage[0].id)) {
+    if(!currentPage[0]) throw new Error("can't find .page element");
+    var newCurrentPage = $(hash); 
+
+    if(newCurrentPage[0] && (!currentPage[0] || hash.slice(1) !== currentPage[0].id)) {
       currentPage.removeClass("page-current");
-      $(hash).addClass("page-current");
+      newCurrentPage.addClass("page-current");
       this.pushstate(hash);
     } else {
       this.pushstate("#"+currentPage[0].id);
     }
 
-    this.urlIDMap = {};
+    window.addEventListener('popstate', $.proxy(this.onpopstate, this));
   }
 
   //加载一个页面,传入的参数是页面id或者url
@@ -27,6 +34,7 @@
     this.getPage(url, function(page) {
       this.animatePages(this.getCurrentPage(), page);
       this.pushstate(url);
+      page.trigger("pageInit", {url: url});
     });
   }
 
@@ -107,8 +115,11 @@
         var html  = response.match(/<body[^>]*>([\s\S.]*)<\/body>/i)[1];
         if(!html) html = response;
         html = "<div>"+html+"</div>";
-        var $page = $(html).find(".page");
+        var tmp = $(html);
+        var $page = tmp.find(".page");
+        if(!$page[0]) $page = tmp.addClass("page");
         $page.insertAfter($(".page")[0]);
+        if(!$page[0].id) $page[0].id = this.genRandomID();
         this.urlIDMap[url] = "#"+$page[0].id;
         callback.apply(this, [$page]);
       }, this)
@@ -126,13 +137,23 @@
   Router.prototype.setCurrentStateID = function(id) {
     this.state.setItem("currentStateID", id);
   }
+  Router.prototype.genRandomID = function() {
+    return "page-"+(+new Date());
+  }
 
   $(function() {
     var router = new Router();
     $(document).on("click", "a", function(e) {
-      e.preventDefault();
-      var $target = $(e.target);
+      var $target = $(e.currentTarget);
+      if($target.hasClass("external") ||
+         $target.attr("external") ||
+         $target.hasClass("tab-link") ||
+         $target.hasClass("open-popup") ||
+         $target.hasClass("open-panel")
+        ) return;
       var url = $target.attr("href");
+      if(!url || url === "#") return;
+      e.preventDefault();
       router.loadPage(url);
     })
   });
