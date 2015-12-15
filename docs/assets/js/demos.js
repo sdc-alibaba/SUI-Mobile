d4b5c376bd3647bc57a2530815e88609a02ba4ec
maqiuyan <qiuyan.mqy@alibaba-inc.com>
2015-12-15 22:10:59 +0800

    测试

--- docs/assets/js/demos.js
+++ docs/assets/js/demos.js
@@ -55,6 +55,10 @@ $(function () {
   
   //顶部无限滚动
   $(document).on("pageInit", "#page-infinite-scroll-top", function(e, id, page) {
+    $(".content").scroller({
+      type : 'js'
+    });
+    $.refreshScroller();
     function addItems(number, lastIndex) {
       // 生成新条目的HTML
       var html = '';
