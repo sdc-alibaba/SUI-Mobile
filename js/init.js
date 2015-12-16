afc9090d4be7769500110d20a8461b0403f4a51a
白荻 <jijin.djj@alibaba-inc.com>
2015-12-16 15:58:03 +0800

    format with editorconfig

--- js/init.js
+++ js/init.js
@@ -1,19 +1,21 @@
 /* global Zepto:true */
 /*jshint unused: false*/
-+function ($) {
++function($) {
     "use strict";
 
     var getPage = function() {
         var $page = $(".page-current");
-        if(!$page[0]) $page = $(".page").addClass("page-current");
+        if (!$page[0]) $page = $(".page").addClass("page-current");
         return $page;
     };
 
     //初始化页面中的JS组件
     $.initPage = function(page) {
         var $page = getPage();
-        if(!$page[0]) $page = $(document.body);
-        var $content = $page.hasClass("content") ? $page : $page.find(".content");
+        if (!$page[0]) $page = $(document.body);
+        var $content = $page.hasClass("content") ?
+                       $page :
+                       $page.find(".content");
         $content.scroller();  //注意滚动条一定要最先初始化
 
         $.initPullToRefresh($content);
@@ -21,28 +23,28 @@
         $.initCalendar($content);
 
         //extend
-        if($.initSwiper) $.initSwiper($content);
+        if ($.initSwiper) $.initSwiper($content);
     };
 
-  if($.smConfig.showPageLoadingIndicator) {
-    //这里的 以 push 开头的是私有事件，不要用
-    $(window).on("pageLoadStart", function() {
-      $.showIndicator();
-    });
-    $(document).on("pageAnimationStart", function() {
-      $.hideIndicator();
-    });
-    $(window).on("pageLoadCancel", function() {
-      $.hideIndicator();
-    });
-    $(window).on("pageLoadComplete", function() {
-      $.hideIndicator();
-    });
-    $(window).on("pageLoadError", function() {
-      $.hideIndicator();
-      $.toast("加载失败");
-    });
-  }
+    if ($.smConfig.showPageLoadingIndicator) {
+        //这里的 以 push 开头的是私有事件，不要用
+        $(window).on("pageLoadStart", function() {
+            $.showIndicator();
+        });
+        $(document).on("pageAnimationStart", function() {
+            $.hideIndicator();
+        });
+        $(window).on("pageLoadCancel", function() {
+            $.hideIndicator();
+        });
+        $(window).on("pageLoadComplete", function() {
+            $.hideIndicator();
+        });
+        $(window).on("pageLoadError", function() {
+            $.hideIndicator();
+            $.toast("加载失败");
+        });
+    }
 
     $.init = function() {
         var $page = getPage();
@@ -52,7 +54,7 @@
     };
 
     $(function() {
-        if($.smConfig.autoInit) {
+        if ($.smConfig.autoInit) {
             $.init();
         }
 
